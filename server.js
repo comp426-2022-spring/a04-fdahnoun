f970219e3739da8cbac2916d8887be85cd406248
fdahnoun <fdahnoun@email.unc.edu>
2022-04-14 18:01:44 -0400

    de

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ app.use( (req, res, next) => {
       protocol: req.protocol,
       httpversion: req.httpVersion,
       status: res.statusCode,
-      referer: req.headers['referer'],
+      referrer: req.headers['referrer'],
       useragent: req.headers['user-agent']
   }
   console.log(logdata)
-  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
   next();
 });
 
@@ -51,21 +51,6 @@ app.get('/app/', (req, res, next) => {
 })
 
 
-
-
-
-// let data = morgan('combined')
-
-// app.use(fs.writeFile('./access.log', data,
-//  {flag: 'a'}, (err, req, res, next) => {
-//    if (err){
-//      console.error(err)
-//    } else {
-//      console.log(data)
-//    }
-//  }
- 
-//  ))
 
 app.get('/app/flip/', (req, res) => {
    var flip = coinFlip()
